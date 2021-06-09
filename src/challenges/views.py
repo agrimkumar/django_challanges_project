0a64755da76c007e3710ca06116fd1ee90242f02
Agrim Kumar <agrim4sure@gmail.com>
2021-06-09 20:29:25 +0530

    Added links in one page to view different months challenges.

--- src/challenges/views.py
+++ src/challenges/views.py
@@ -1,24 +1,40 @@
 from django.shortcuts import render
-from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect
-
+from django.http import HttpResponse, HttpResponseNotFound, HttpResponseRedirect, response
+from django.urls import reverse
 
 monthly_challenges = {
-    'january' : '<h1> Eat no meat for this month </h1>',
-    'february' : '<h1> Walk for atleast 15 min this month </h1>',
-    'march' : '<h1> Learn Django for atleast 1 hour every day! </h1>',
-    'april' : '<h1> Meditate for atleast 15 minutes every day! </h1>',
-    'may' : '<h1> Workout for atleast 30 minutes every day! </h1>',
+    'january' : 'Eat no meat for this month',
+    'february' : 'Walk for atleast 15 min this month',
+    'march' : 'Learn Django for atleast 1 hour every day!',
+    'april' : 'Meditate for atleast 15 minutes every day!',
+    'may' : 'Workout for atleast 30 minutes every day!',
 }
+
+def index(request):
+    list_items = ''
+    months = list(monthly_challenges.keys())
+
+    for month in months:
+        capitalized_month = month.capitalize()
+        month_path = reverse('month-challenge', args=[month])
+        list_items += f'<li> <a href="{month_path}"> {capitalized_month} </a> </li>'
+    
+    response_data = f'<ul> {list_items} </ul>'
+    return HttpResponse(response_data)
 
 def monthly_challenge_by_number(request, month):
     months = list(monthly_challenges.keys())
     redirect_month = months[month-1]
-    return HttpResponseRedirect('/challenges/' + redirect_month)
+    redirect_path = reverse('month-challenge', args=[redirect_month]) # /challenge/january
+    return HttpResponseRedirect(redirect_path)
 
 
 def monthly_challenge(request, month):
     try:
-        challenge_text =  monthly_challenges[month]
-        return HttpResponse(challenge_text)
+        challenge_text = monthly_challenges[month]
+        response_data = f'<h1>{challenge_text}</h1>'
+        return HttpResponse(response_data)
     except:
         return HttpResponseNotFound('<h1> This month is not supported </h1>')
+
+
