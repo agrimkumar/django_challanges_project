adc4b0bf61b5d801fd4d5251eeca914074aa4eda
Agrim Kumar <agrim4sure@gmail.com>
2021-06-09 17:59:47 +0530

    Added first view.

--- src/monthly_challenges/urls.py
+++ src/monthly_challenges/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('challenges.urls')),
+    path('challenges/', include('challenges.urls')),
 ]
